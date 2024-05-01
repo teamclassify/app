f52989f52f0e7a9423ea6647710e12d48ae4bcc1
Andres Parra <byandrev@gmail.com>
2024-05-01 16:58:23 -0500

    set Wrapper responsive

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,13 +4,15 @@ import DrawerMenu from './Drawer'
 import Header from './Header'
 
 function Wrapper ({ children }) {
-  return <Grid gridTemplateColumns="0.14fr 1fr">
+  return <Grid gridTemplateColumns={{ base: '1fr', md: '0.14fr 1fr' }}>
     <DrawerMenu />
 
     <Box px={4}>
       <Header />
 
-      {children}
+      <Box mt={4}>
+        {children}
+      </Box>
     </Box>
 </Grid>
 }
