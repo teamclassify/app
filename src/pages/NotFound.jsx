2cd9a494d2d168ddb48864a40fbf4b46bac6700c
Andres Parra <byandrev@gmail.com>
2024-05-05 12:05:14 -0500

    feat: create 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,36 @@
+import { Box, Button, Center, Heading, Image, Text } from '@chakra-ui/react'
+import { Link } from 'wouter'
+
+import Wrapper from '@/components/Wrapper'
+
 function NotFound () {
-  return <>404</>
+  return (
+    <Wrapper>
+      <Center flexDir="column" gap={8}>
+        <Image src="/assets/not-found.svg" w="100%" maxW="300px" />
+        <Box textAlign="center">
+          <Heading as="h2" size="lg" mb={2}>
+            Página no encontrada
+          </Heading>
+
+          <Text mb={4}>
+            Parece que estás buscando una página que no existe en nuestra
+            aplicación
+          </Text>
+
+          <Button
+            as={Link}
+            href="/"
+            size="sm"
+            variant="outline"
+            colorScheme="primary"
+          >
+            Volver al Inicio
+          </Button>
+        </Box>
+      </Center>
+    </Wrapper>
+  )
 }
 
 export default NotFound
