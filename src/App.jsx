cb0ee51c994b383c70cb649fc52b80a0f6b5fc58
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-05-07 15:22:13 -0500

    feat: create login user page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'wouter'
 import HomePage from './pages/Home'
 import LoanPage from './pages/Loan'
 import LoginPage from './pages/Login'
+import LoginUserPage from './pages/Login-Super-User'
 import ErrorPage from './pages/NotFound'
 import UserPage from './pages/User'
 import UpdatePage from './pages/UploadPDF'
@@ -15,6 +16,7 @@ function App () {
       <Switch>
         <Route component={HomePage} path="/" />
         <Route component={LoginPage} path="/login" />
+        <Route component={LoginUserPage} path="/login-super-user" />
         <Route component={UserPage} path="/home" />
         <Route component={UpdatePage} path="/upload" />
         <Route component={BuildingsPage} path="/edificios" />
