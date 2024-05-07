a38bef635dd6df17713b804a7c6ad3c0972fcff2
Andres Parra <byandrev@gmail.com>
2024-05-07 15:39:39 -0500

    feat: improve drawer component

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,41 +1,111 @@
-import {
-  Box,
-  Button,
-  Heading
-} from '@chakra-ui/react'
-
+import { Box, Button, Divider, Heading } from '@chakra-ui/react'
 import { FaComputer } from 'react-icons/fa6'
 import { GrSchedule } from 'react-icons/gr'
 import { IoIosSettings } from 'react-icons/io'
 import { IoPersonSharp } from 'react-icons/io5'
-import { MdDashboard } from 'react-icons/md'
 import { RiLogoutBoxLine } from 'react-icons/ri'
-import UserPic from './UserPic'
+import { Link } from 'wouter'
 
 import useUser from '../hooks/useUser'
-import { Link } from 'wouter'
+import UserPic from './UserPic'
 
 function DrawerForm () {
   const { logout } = useUser()
 
   return (
-    <>
-      <Box p={8} pt={16} display='flex' flexDirection='column' bg="rgb(196 38 38)" minH="100vh" justifyContent='space-between' alignItems='start'>
-        <Box>
-          <Box display='flex'justifyContent='center' pb={10}>
-            <UserPic/>
-            </Box>
-          <Button colorScheme='red' bg='transparent' gap={2}><IoPersonSharp size='13px'color='white' /><Link href='/home'><Heading as='h1' size='sl' py={2}>Inicio</Heading></Link></Button>
-          <Button colorScheme='red' bg='transparent' gap={2}><GrSchedule size='13px'color='white' /><Link href=''> <Heading as='h1' size='sl' color='white' py={2}>Horario</Heading></Link></Button>
-          <Button colorScheme='red' bg='transparent' gap={2}><MdDashboard size='13px'color='white' /><Link href=''><Heading as='h1' size='sl' color='white' py={2}>Dashboard</Heading></Link></Button>
-          <Button colorScheme='red' bg='transparent' gap={2}><FaComputer size='13px'color='white' /><Link href='/prestamos'><Heading as='h1' size='sl' color='white' py={2}>Prestamos</Heading></Link></Button>
-         </Box>
-         <Box>
-         <Button colorScheme='red' bg='transparent' gap={2}><IoIosSettings size='13px'color='white' /><Link href=''><Heading as='h1' size='sl' color='white' py={2}>Ajustes</Heading></Link></Button>
-         <Button onClick={logout} colorScheme='red' bg='transparent' gap={2}><RiLogoutBoxLine size='13px'color='white' /><Heading as='h1' size='sl' color='white' py={2}>Logout</Heading></Button>
-         </Box>
+    <Box
+      p={4}
+      minH="100vh"
+      display="flex"
+      bg="rgb(196 38 38)"
+      alignItems="start"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Box>
+        <Box display="flex" justifyContent="center" mb={6}>
+          <UserPic />
+        </Box>
+
+        <Divider mb={4} borderColor='primary.400' borderWidth={1} />
+
+        <Button
+          gap={2}
+          w="full"
+          bg="transparent"
+          colorScheme="red"
+          justifyContent="start"
+        >
+          <IoPersonSharp size="13px" color="white" />
+          <Link href="/home">
+            <Heading as="h1" size="sl" py={2}>
+              Inicio
+            </Heading>
+          </Link>
+        </Button>
+
+        <Button
+          gap={2}
+          w="full"
+          bg="transparent"
+          colorScheme="red"
+          justifyContent="start"
+        >
+          <GrSchedule size="13px" color="white" />
+          <Link href="/horario">
+            <Heading as="h1" size="sl" color="white" py={2}>
+              Horario
+            </Heading>
+          </Link>
+        </Button>
+
+        <Button
+          gap={2}
+          w="full"
+          bg="transparent"
+          colorScheme="red"
+          justifyContent="start"
+        >
+          <FaComputer size="13px" color="white" />
+          <Link href="/prestamos">
+            <Heading as="h1" size="sl" color="white" py={2}>
+              Prestamos
+            </Heading>
+          </Link>
+        </Button>
       </Box>
-    </>
+
+      <Box>
+        <Button
+          gap={2}
+          w="full"
+          bg="transparent"
+          colorScheme="red"
+          justifyContent="start"
+        >
+          <IoIosSettings size="13px" color="white" />
+          <Link href="">
+            <Heading as="h1" size="sl" color="white" py={2}>
+              Ajustes
+            </Heading>
+          </Link>
+        </Button>
+
+        <Button
+          gap={2}
+          w="full"
+          onClick={logout}
+          bg="transparent"
+          colorScheme="red"
+          justifyContent="start"
+        >
+          <RiLogoutBoxLine size="13px" color="white" />
+          <Heading as="h1" size="sl" color="white" py={2}>
+            Logout
+          </Heading>
+        </Button>
+      </Box>
+    </Box>
   )
 }
 
