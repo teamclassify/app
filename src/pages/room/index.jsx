1cbbf5722775adbd836f6f461bb7a7524a9ad883
Andres Parra <byandrev@gmail.com>
2024-05-08 20:44:06 -0500

    refactor: Remove SelectRooms component from Room page

--- src/pages/room/index.jsx
+++ src/pages/room/index.jsx
@@ -33,7 +33,6 @@ function Room () {
               currentBuilding={currentBuilding}
               setCurrentBuilding={setCurrentBuilding}
             />
-            <SelectRooms/>
           </Flex>
 
           <Flex alignItems="center" gap={2} bg='white' px={2} py={1} rounded='md'>
