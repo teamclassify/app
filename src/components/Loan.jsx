d98f71a3db4af54fe33e105ad34f419683b8930b
MauricioDDS <mauriciodidonato794@gmail.com>
2024-05-09 12:45:47 -0500

    chore: Modularize Loans and MyLoans

--- src/components/Loan.jsx
+++ src/components/Loan.jsx
@@ -1,32 +1,62 @@
-import { LinkBox, LinkOverlay, Box, Heading, Text } from '@chakra-ui/react'
-import { FaDoorOpen } from 'react-icons/fa'
+import {
+  LinkBox,
+  LinkOverlay,
+  Box,
+  Heading,
+  Badge
+} from '@chakra-ui/react'
+import { FaDoorOpen, FaDoorClosed } from 'react-icons/fa'
 import { Link } from 'wouter'
+import ToolTipo from './ToolTip'
 
-function Loan () {
-  /* const { title, status } = 'Not Set'
-  const date = '12-31-2077' */
-
-  return (<>
-    <LinkBox
+function Loan ({ Room, Date, State }) {
+  const backgroundColor = State === 1 ? '#1B9C4A' : State === 0 ? '#EDD038' : '#D82D2D'
+  let icon
+  if (State === 1) {
+    icon = <FaDoorOpen size="50px" color="white" />
+  } else {
+    icon = <FaDoorClosed size="50px" color="white" />
+  }
+  return (
+    <>
+      <LinkBox
         textAlign="center"
         maxW="sm"
         p="8"
-        rounded='xl'
-        backgroundColor="rgb(27 156 75)"
-        display='flex'
-        flexDirection='column'
-        width='14rem'
+        rounded="xl"
+        backgroundColor={backgroundColor}
+        display="flex"
+        flexDirection="column"
+        width="14rem"
       >
-          <LinkOverlay as={Link} href="">
-          <Box display='flex' flexDirection='row' justifyContent='space-between'>
-            <FaDoorOpen size="50px" color="white" ></FaDoorOpen>
-            </Box>
-            <Box display='flex' flexDirection='column' mt={2} alignItems='start'>
-            <Heading fontWeight='bold' size="md" my="2" fontFamily="sans-serif" color='white'>SA414</Heading>
-            <Text color='white' fontFamily='sans-serif'>28/4/2024</Text></Box>
-          </LinkOverlay>
-    </LinkBox>
-  </>)
+        <LinkOverlay as={Link} href="">
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            {icon}
+            <ToolTipo State={State}/>
+          </Box>
+          <Box display="flex" flexDirection="column" mt={2} alignItems="start">
+            <Heading
+              fontWeight="bold"
+              size="md"
+              my="2"
+              fontFamily="sans-serif"
+              color="white"
+            >
+              {Room}
+            </Heading>
+            <Badge bg='#ffffff24' color="white" fontFamily="sans-serif">
+              {Date}
+            </Badge>
+          </Box>
+        </LinkOverlay>
+      </LinkBox>
+    </>
+  )
 }
 
 export default Loan
--- src/components/MyLoans.jsx
+++ src/components/MyLoans.jsx
@@ -2,18 +2,27 @@ import { Box, Heading, SimpleGrid } from '@chakra-ui/react'
 import Loan from './Loan'
 
 function MyLoans () {
-  return (<>
-    <Box display='flex' flexDirection='column' mt='2rem'>
-      <Box display='flex' alignContent='center' alignItems='center' justifyContent='start'>
-      <Heading fontSize='1.2rem' fontFamily='sans-serif'>Prestamos Recientes</Heading>
+  return (
+    <>
+      <Box display="flex" flexDirection="column" mt="2rem">
+        <Box
+          display="flex"
+          alignContent="center"
+          alignItems="center"
+          justifyContent="start"
+        >
+          <Heading fontSize="1.2rem" fontFamily="sans-serif">
+            Prestamos Recientes
+          </Heading>
+        </Box>
+        <SimpleGrid display="flex" flexDirection="row" spacing="4" mt="10px">
+          <Loan Room='SA 414' Date="9/5/2024" State={1}/>
+          <Loan Room='SB 404' Date="7/5/2024" State={0}/>
+          <Loan Room='SA 402' Date="19/5/2024" State={-1}/>
+        </SimpleGrid>
       </Box>
-      <SimpleGrid display='flex' flexDirection='row' spacing='4' mt='10px'>
-        <Loan></Loan>
-        <Loan></Loan>
-        <Loan></Loan>
-      </SimpleGrid>
-    </Box>
-  </>)
+    </>
+  )
 }
 
 export default MyLoans
