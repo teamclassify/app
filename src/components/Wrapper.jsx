24e1ffc88b5809470a186d1b7d4a5aa8407af360
Andres Parra <byandrev@gmail.com>
2024-05-09 10:13:20 -0500

    refactor: Improve user loading experience with LoadingUser component

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,29 +1,25 @@
-import { Grid, Box, Spinner, Center } from '@chakra-ui/react'
+import { Box, Grid } from '@chakra-ui/react'
 
+import useUser from '../hooks/useUser'
+import LoadingUser from '../pages/LoadingUser'
 import DrawerMenu from './Drawer'
 import Header from './Header'
-import useUser from '../hooks/useUser'
 
 function Wrapper ({ children }) {
   const { loading } = useUser()
 
+  if (loading) {
+    return <LoadingUser />
+  }
+
   return (
-    <Grid h='100vh' gridTemplateColumns={{ base: '1fr', md: '0.2fr 1fr' }}>
+    <Grid h="100vh" gridTemplateColumns={{ base: '1fr', md: '0.2fr 1fr' }}>
       <DrawerMenu />
 
-      <Box px={4} overflowY='auto'>
+      <Box px={4} overflowY="auto">
         <Header />
 
-        {loading
-          ? (
-          <Center minH="90vh">
-            <Spinner size="xl" />
-          </Center>
-
-            )
-          : (
-          <Box mt={0}>{children}</Box>
-            )}
+        <Box mt={0}>{children}</Box>
       </Box>
     </Grid>
   )
