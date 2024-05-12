b4a9f5d79574d13ec5e4294b7f8bfa1701dc7e2d
Andres Parra <byandrev@gmail.com>
2024-05-12 11:09:38 -0500

    refactor: Update ListOfLoans component to use auto-fill for responsive grid layout

--- src/components/ListOfLoans.jsx
+++ src/components/ListOfLoans.jsx
@@ -42,7 +42,7 @@ function ListOfLoans () {
         <Grid
           w="full"
           gap={2}
-          templateColumns="repeat(auto-fit, minmax(10rem, 1fr))"
+          templateColumns="repeat(auto-fill, minmax(10rem, 1fr))"
         >
           {data &&
             data.data &&
