b41ec7ad8203e3daf932fb31bcb916f23c9567e3
Andres Parra <byandrev@gmail.com>
2024-05-12 09:40:47 -0500

    refactor: Update RoomNotFound component to display warning icon and improve heading style

--- src/pages/room/RoomNotFound.jsx
+++ src/pages/room/RoomNotFound.jsx
@@ -1,9 +1,12 @@
 import { Center, Heading, Text } from '@chakra-ui/react'
+import { IoWarningOutline } from 'react-icons/io5'
 
 function RoomNotFound () {
   return (
     <Center flexDir="column" minH="calc(100vh - 100px)">
-      <Heading mb={2} as="h2" size="lg">
+      <IoWarningOutline size='100px' />
+
+      <Heading my={2} as="h2" size="lg">
         No se encuentra la sala
       </Heading>
       <Text mb={6}>La sala que estas buscando no existe por el momento.</Text>
