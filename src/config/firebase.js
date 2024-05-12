548352e15ea88dd8feeb58159ef049a6aa97f29c
Andres Parra <byandrev@gmail.com>
2024-05-12 09:18:16 -0500

    refactor: Update firebase.js to set custom parameters for GoogleAuthProvider

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -16,6 +16,7 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 const providerGoogle = new GoogleAuthProvider()
+providerGoogle.setCustomParameters({ hd: 'ufps.edu.co' })
 
 const auth = getAuth(app)
 
