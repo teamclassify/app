e91f677e2ed3874c7c96d09b3d48682e31a5e43e
Andres Parra <byandrev@gmail.com>
2024-05-17 17:10:39 -0500

    refactor: Add loading state to User component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,10 +4,15 @@ import MyLoans from '../components/MyLoans'
 import UserOptions from '../components/UserOptions'
 import Wrapper from '../components/Wrapper'
 import useUser from '../hooks/useUser'
+import LoadingUser from './LoadingUser'
 import NotAuth from './NotAuth'
 
 function User () {
   const { user, loading } = useUser()
+
+  if (loading) {
+    return <LoadingUser />
+  }
 
   if (!loading && !user) {
     return <NotAuth />
