49bd77fdf877b0029e7c21dba8b7e338f4452406
Andres Parra <byandrev@gmail.com>
2024-06-01 17:19:26 -0500

    feat: create FilterSelect component

---
+++ src/components/FilterSelect.jsx
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { FaAngleDown } from 'react-icons/fa'
+
+function FilterSelect (
+  { title, size, options, onCheck } = { size: 'md', options: [] }
+) {
+  const [checkedItems, setCheckedItems] = useState(
+    options.map((option) => {
+      return {
+        value: option,
+        checked: false
+      }
+    })
+  )
+
+  const handleCheck = (index, value, isChecked) => {
+    setCheckedItems((prev) => {
+      const newValues = [...prev]
+      newValues[index] = { value, checked: isChecked }
+      onCheck(newValues)
+      return newValues
+    })
+  }
+
+  return (
+    <Box>
+      <Popover placement="bottom-start">
+        <PopoverTrigger>
+          <Button size={size} rightIcon={<FaAngleDown />} colorScheme="gray">
+            {title}
+          </Button>
+        </PopoverTrigger>
+
+        <PopoverContent maxW="170px">
+          <PopoverBody>
+            <FormControl>
+              {options.map((option, index) => {
+                return (
+                  <Checkbox
+                    key={index}
+                    value={option}
+                    isChecked={checkedItems[index]?.checked ?? false}
+                    onChange={(e) =>
+                      handleCheck(index, option, e.target.checked)
+                    }
+                  >
+                    {option}
+                  </Checkbox>
+                )
+              })}
+            </FormControl>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </Box>
+  )
+}
+
+export default FilterSelect
