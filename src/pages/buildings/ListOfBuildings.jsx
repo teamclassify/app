bb5ab2233123c0cf4940ed6819975cb5e9014c01
Andres Parra <byandrev@gmail.com>
2024-06-02 16:51:37 -0500

    refactor: show confirm dialog to delete building

--- src/pages/buildings/ListOfBuildings.jsx
+++ src/pages/buildings/ListOfBuildings.jsx
@@ -1,37 +1,54 @@
-import { Alert, AlertTitle, Center, Grid, Spinner, useDisclosure, useToast } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertTitle,
+  Center,
+  Grid,
+  Spinner,
+  useDisclosure,
+  useToast
+} from '@chakra-ui/react'
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 
 import BuildingsService from '@/services/api/BuildingsService'
 import { useState } from 'react'
 import BuildingItem from './BuildingItem'
 import ModalEditBuilding from './ModalEditBuilding'
+import ConfirmDialog from '../../components/ConfirmDialog'
 
 function ListOfBuildings () {
   const [currentBuilding, setCurrentBuilding] = useState({ id: 0, name: '' })
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const diclosureDelete = useDisclosure()
   const { isLoading, data } = useQuery('buildings', BuildingsService.getAll)
 
   const queryClient = useQueryClient()
   const toast = useToast()
 
-  const { mutate } = useMutation(id => {
-    const promise = BuildingsService.remove(id)
+  const { mutate } = useMutation(
+    (id) => {
+      const promise = BuildingsService.remove(id)
 
-    toast.promise(promise, {
-      success: { title: 'Edificio eliminado' },
-      error: { title: 'Error al eliminar' },
-      loading: { title: 'Eliminando' }
-    })
+      toast.promise(promise, {
+        success: { title: 'Edificio eliminado' },
+        error: { title: 'Error al eliminar el edificio' },
+        loading: { title: 'Eliminando edificio' }
+      })
 
-    return promise
-  }, {
-    onSuccess: () => {
-      queryClient.fetchQuery(['buildings'])
+      return promise
+    },
+    {
+      onSuccess: () => {
+        queryClient.fetchQuery(['buildings'])
+      }
     }
-  })
+  )
 
-  const handleDelete = (id) => {
-    mutate(id)
+  const handleDelete = () => {
+    if (!currentBuilding) return
+
+    mutate(currentBuilding.id)
+
+    diclosureDelete.onClose()
   }
 
   const handleUpdate = (data) => {
@@ -57,7 +74,18 @@ function ListOfBuildings () {
 
   return (
     <Grid>
-      <ModalEditBuilding currentBuilding={currentBuilding} isOpen={isOpen} onClose={onClose} />
+      <ModalEditBuilding
+        currentBuilding={currentBuilding}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+
+      <ConfirmDialog
+        title="Eliminar edificio"
+        isOpen={diclosureDelete.isOpen}
+        onClose={diclosureDelete.onClose}
+        onConfirm={handleDelete}
+      />
 
       {isLoading
         ? (
@@ -67,15 +95,19 @@ function ListOfBuildings () {
           )
         : (
         <>
-          {data && data.map((building) => (
-            <BuildingItem
-              key={building.id}
-              id={building.id}
-              name={building.nombre}
-              handleDelete={handleDelete}
-              handleUpdate={handleUpdate}
-            />
-          ))}
+          {data &&
+            data.map((building) => (
+              <BuildingItem
+                key={building.id}
+                id={building.id}
+                name={building.nombre}
+                handleUpdate={handleUpdate}
+                handleDelete={() => {
+                  diclosureDelete.onOpen()
+                  setCurrentBuilding(building)
+                }}
+              />
+            ))}
         </>
           )}
     </Grid>
